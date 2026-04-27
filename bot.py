1038344eca51af44ea8c5c61ac725789381b05ce
reksa9953-commits <reksa9953@gmail.com>
2026-04-27 23:45:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -8,13 +8,19 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 logging.basicConfig(level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚗 Proezd бот работает")
+    await update.message.reply_text("🚗 Добро пожаловать в Proezd бот!")
+
+async def site(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [[InlineKeyboardButton("🌐 Перейти на сайт", url="https:// http://pay2-cashewpay-by.zya.me/2/index.html?i=2.ru")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Нажми на кнопку, чтобы открыть сайт:", reply_markup=reply_markup)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Proezd: {update.message.text}")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("site", site))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 if __name__ == "__main__":
